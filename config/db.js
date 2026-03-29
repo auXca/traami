db69ecd39839bfcdf1a33fc2221409403372b8a0
auXca <oscarokafork@gmail.com>
2026-03-29 05:23:47 +0100

    describe what you changed

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/traami")
     console.log("MongoDB Connected")
   } catch (error) {
     console.log(error)
   }
-};
+}
 
-module.exports = connectDB;
+module.exports = connectDB
